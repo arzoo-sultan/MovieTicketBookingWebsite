120005c21436f26e6b357b9f0b3c0d8084ed549c
arzoo-sultan <meers2800@gmail.com>
2026-06-22 22:10:49 +0500

    feat: Add movie booking features with date select and trailer components
    Add new DateSelect and TrailerSection components, enhance MovieCard and
    MovieDetails pages, and improve overall styling across the application

--- client/src/components/FeatureSection.jsx
+++ client/src/components/FeatureSection.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { ArrowRight } from 'lucide-react';
 import BlurCircle from './BlurCircle';
+import MovieCard from './MovieCard';
+import { dummyShowsData } from '../assets/assets';
 
 const FeatureSection = () => {
     const navigate=useNavigate();
@@ -12,7 +14,11 @@ const FeatureSection = () => {
              <p className='text-gray-300 font-medium text-lg '> Now Showing</p>
             <button onClick={()=>navigate( '/movies')} className='group flex items-center gap-2 text:sm text-gray-300 cursor-pointer'>View All <ArrowRight className='group-hover:translate-x-0.5 transition w-4.5 h-4.5 '/></button>
         </div>
-        <div></div>
+        <div className='flex flex-wrap max-sm:justify-center gap-8 mt-8'>
+          {dummyShowsData.slice(0,4).map((show)=>(
+            <MovieCard key={show._id} movie={show}/>
+          ))}
+        </div>
         <div className='flex justify-center mt-60'>
             <button onClick={()=>{navigate('/movies');scrollTo(0,0)}}
              className=' px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded md: font-medium cursor-pointer'>Show More</button>
